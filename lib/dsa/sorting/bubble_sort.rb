91141d5aa5f9733a4967852bd26f5263d3775b38
Tira Sundara <sundaralinus@gmail.com>
2021-09-30 20:44:04 +0700

    Add Sorting BubbleSort Class

---
+++ lib/dsa/sorting/bubble_sort.rb
@@ -0,0 +1,38 @@
+module DSA
+  module Sorting
+
+    # Bubble Sort Algorithm
+    # Time: O(N^2)
+    class BubbleSort < Base
+      attr_reader :nums
+
+      def initialize(nums)
+        @nums = nums
+      end
+
+      def sort
+        sort_until_index = nums.size - 1
+        is_sorted = false
+
+        while !is_sorted do
+          is_sorted = true
+
+          (0...sort_until_index).each do |i|
+            if nums[i] > nums[i+1]
+              nums[i], nums[i+1] = nums[i+1], nums[i]
+              is_sorted = false
+            end
+          end
+
+          sort_until_index -= 1
+        end
+
+        nums
+      end
+
+      private
+
+      attr_writer :nums
+    end
+  end
+end
