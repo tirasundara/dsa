d02ee2dabc58fb1ef24c62d525a247b9a3a7dbe8
Tira Sundara <sundaralinus@gmail.com>
2021-09-30 20:44:25 +0700

    Add Sorting SelectionSort Class

---
+++ lib/dsa/sorting/selection_sort.rb
@@ -0,0 +1,39 @@
+module DSA
+  module Sorting
+
+    # Selection Sort Algorithm
+    # Time: O(N^2)
+    # Even though it's O(N^2), but actually it's half-faster than the Bubble Sort
+    class SelectionSort < Base
+      attr_reader :nums
+
+      def initialize(nums)
+        @nums = nums
+      end
+
+      def sort
+        return nums if nums.size.zero?
+
+        (0...nums.size).each do |i|
+          lowest_number_index = i
+
+          (i+1...nums.size).each do |j|
+            if nums[j] < nums[lowest_number_index]
+              lowest_number_index = j
+            end
+          end
+
+          if lowest_number_index != i
+            nums[i], nums[lowest_number_index] = nums[lowest_number_index], nums[i]
+          end
+        end
+
+        nums
+      end
+
+      private
+
+      attr_writer :nums
+    end
+  end
+end
