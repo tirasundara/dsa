d928e70659c8605d86f4212f590136ec88b5b8a9
Tira Sundara <sundaralinus@gmail.com>
2021-10-01 10:07:30 +0700

    Add BubbleSort spec

---
+++ spec/dsa/sorting/bubble_sort_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe ::DSA::Sorting::BubbleSort do
+  describe "#sort" do
+    it "returns sorted array" do
+      nums = [4, 13, 51, 43, 5345, 1231, 1, 6, 7]
+
+      expect(described_class.new(nums).sort).to eq([1, 4, 6, 7, 13, 43, 51, 1231, 5345])
+    end
+  end
+end
